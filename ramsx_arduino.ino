dfaff7c0428327c2f3086735ae774fc8bcab9dc3
Yassir Al-Refaie <yassir.alrefaie@gmail.com>
2023-04-29 08:31:05 +0100

    Programming appears to work for 8k & 16k roms

--- ramsx_arduino.ino
+++ ramsx_arduino.ino
@@ -25,30 +25,32 @@ typedef FsFile file_t;
 #error Invalid SD_FAT_TYPE
 #endif  // SD_FAT_TYPE
 
+#define CONTROL_A16 0
+#define CONTROL_nREAD 1
+#define CONTROL_nWRITE 2
+#define CONTROL_SEL_A 3
+#define CONTROL_SEL_B 4
+#define CONTROL_HANDOVER 5
+#define CONTROL_COMM_RESET 6
 
-void testRead(){
-   for(int i=0;i<30;i++){
-    byte data = readDataFromAddress(0x4000+i);
-    Serial.print(data,HEX);
-  }
-  // handover();
-  while(true);
-  
-}
+
+uint8_t controlStatus = 0;
 
 void setup() {
+
+  setupControlPins();
+  haltMSX();
   Serial.begin(115200);
   Serial.print("\n******* Starting New Run *******\n");
-  assertReset();
   
-  // testRead();
+  setupDataPinsForWrite();
   
-  // while (!Serial)
-  //   ;
+  setControlForBootloaderWrite();
 
+  
   sd_t sd = initializeSDCard(10);
   sd.setDedicatedSpi(true);
-  Serial.print("\nSD Card Initialized");
+  
   
   file_t root;
   file_t romFile;
@@ -58,47 +60,182 @@ void setup() {
   }
   // char* filename = "Tank Battalion (1984)(Namcot)(JP).rom";
   // char* filename = "testrom_write.rom";
-  if(!romFile.open(&root, "Pac-Man (1984)(Namcot)(JP).rom", O_READ)){
+  if(!romFile.open(&root, "out.rom", O_READ)){
     Serial.print("\nERROR: Error Opening File");
     sd.errorHalt(&Serial);
   }
-  writeFileToSRAM(romFile);
-  // displayDirectoryContent(sd, root, 0);
- 
+  writeFileToSRAM(romFile); 
   handover();
+  releaseMSX();
+  clearReadOrWrite();
   while (true);
 
 }
 
 void loop() {
-  // file_t dir;
-
-  // if(!dir.open("/")){
-  //   Serial.print("\n[ERROR] Error opening Root Dir");
-  //   sd.errorHalt(&Serial);
-  // }
-  // setupPinsForWrite();
-  // // put your main code here, to run repeatedly:
 }
 
-void setupPinsForWrite() {
+
+void latchControl(){
+  PORTC |= B001000;
+  PORTC &= B110111;
+}
+
+void latchLowAddress() {
+  PORTC |= B000001 ;
+  PORTC &= B111110 ;
+}
+
+void latchHighAddress() {
+  PORTC |= B000010;
+  PORTC &= B111101;
+}
+
+void setControlForBootloaderWrite(){
+  Serial.println("Setting up for Bootloader Write");
+  clearControlBit(CONTROL_A16);
+  clearControlBit(CONTROL_nWRITE);
+  clearControlBit(CONTROL_COMM_RESET);
+  setControlBit(CONTROL_nREAD);
+  setControlBit(CONTROL_HANDOVER);
+}
+
+void haltMSX(){
+  PORTC &= B101111;
+}
+
+void releaseMSX(){
+  PORTC |= B010000;
+}
+
+void deassertAddress16(){
+  Serial.println("\n Deasserting A16");
+  clearControlBit(CONTROL_A16);
+}
+
+void assertAddress16(){
+  Serial.println("\n Asserting A16");
+  setControlBit(CONTROL_A16);
+}
+
+
+void setupControlPins(){
+  DDRC = DDRC | B011111;
+}
+void setupDataPinsForWrite() {
   DDRD = DDRD | B11111100;
   DDRB = DDRB | B00000011;
-  DDRC = DDRC | B111111;
-  // Serial.println("\nSetting Port D2-7, B0-1, C0-5 as Outputs");
+  
 }
-void setupPinsForRead() {
+void setupDataPinsForRead() {
   DDRD = DDRD & B00000011;
   DDRB = DDRB & B11111100;
-  DDRC = DDRC | B111111;
-  // Serial.println("\nSetting Port D2-7, B0-1 as Input, C0-5 as Outputs");
 }
-void setDataPinValue(byte data) {
-  // Serial.println(data);
-  // Put lower 6 bits onto port D2-7
-  // Put upper 8 bits onto port B0-1
+void setDataPinsValue(byte data) {
   PORTD = (((data << 2) & 0xFC)) | PORTD & 0x3;
   PORTB = (data >> 6) & 0x3 | PORTB & 0xFC;
+}
+
+
+sd_t initializeSDCard(int pin) {
+  Serial.print("Initializing SD card...\r\n");
+  sd_t sd;
+
+  if (!sd.begin(pin, SPI_FULL_SPEED)) {
+    Serial.println("initialization failed. Things to check:");
+    Serial.println("1. is a card inserted?");
+    Serial.println("2. is your wiring correct?");
+    Serial.println("3. did you change the chipSelect pin to match your shield or module?");
+    Serial.println("Note: press reset or reopen this serial monitor after fixing your issue!");
+    sd.initErrorHalt();
+  }
+  Serial.print("\nSD Card Initialized");
+  return sd;
+}
+
+
+
+void setControlBit(uint8_t controlPin){
+  // Serial.print("Setting Control Bit "); Serial.print(controlPin);Serial.print("\r\n");
+  // Serial.print("Previous Control: B"); Serial.print(controlStatus, BIN);Serial.print("\r\n");
+  controlStatus |= (1<<controlPin);
+  setDataPinsValue(controlStatus);
+  latchControl();
+  // Serial.print("New Control: B"); Serial.print(controlStatus, BIN);Serial.print("\r\n");
+}
+void clearControlBit(uint8_t controlPin){
+  // Serial.print("Clearing Control Bit "); Serial.print(controlPin);Serial.print("\r\n");
+  // Serial.print("Previous Control: B"); Serial.print(controlStatus, BIN);Serial.print("\r\n");
+  controlStatus &= ~(1UL<<controlPin);
+  setDataPinsValue(controlStatus);
+  latchControl();
+  // Serial.print("New Control: B"); Serial.print(controlStatus, BIN);Serial.print("\r\n");
+}
+
+void handover() {
+  Serial.println("\nHanding Over");
+  clearControlBit(CONTROL_HANDOVER);
+}
+
+void selectRAM() {
+  PORTC &= B111011;
+}
+
+void deselectRAM() {
+  PORTC |= B000100;
+}
+
+void assertWrite() {
+  Serial.println("\nAsserting Write");
+  setControlBit(CONTROL_nREAD);
+  clearControlBit(CONTROL_nWRITE);
+}
+
+void assertRead() {
+  Serial.println("\nAsserting Read");
+  clearControlBit(CONTROL_nREAD);
+  setControlBit(CONTROL_nWRITE);
+}
+
+uint8_t readData(){
+  byte data = (PIND & B11111100)>>2;
+  return data | (((PINB & B00000011))<<6);
+}
+
+void clearReadOrWrite(){
+  Serial.println("\nClearing Read and Write");
+  setControlBit(CONTROL_nREAD);
+  setControlBit(CONTROL_nWRITE);
+}
+
+
+uint8_t readDataFromAddress(uint16_t address){
+  deselectRAM();
+  //deassertReadandWrite();
+  setupDataPinsForWrite();
+  byte lowReadAddress = address & 0xFF;
+  byte highReadAddress = (address >> 8) & 0xFF;
+  setDataPinsValue(lowReadAddress);
+  latchLowAddress();
+  setDataPinsValue(highReadAddress);
+  latchHighAddress();
+  assertRead();
+  selectRAM();
+  setupDataPinsForRead();
+  return readData();
+}
+
+void setChipSelect(uint8_t cs){
+  if(cs == 0 ){
+    clearControlBit(CONTROL_SEL_A);
+    clearControlBit(CONTROL_SEL_B);
+  }
+  if(cs & 1 == 1){
+    setControlBit(CONTROL_SEL_A);
+  }
+  if(cs & 2 == 2 ){
+    setControlBit(CONTROL_SEL_B);
+  }
 }
 
 int writeFileToSRAM(const file_t &romFile){
@@ -107,24 +244,27 @@ int writeFileToSRAM(const file_t &romFile){
   romFile.seek(3);
 
   unsigned int offset = 0;
-  char* chipSelect = "CS1";
-  bool needToSwap = false;
+  uint8_t chipSelect = 0;
   byte peekValue = romFile.peek();
+
   if (peekValue >= 0x80) {
     offset = 0x8000;
-    chipSelect = "CS2";
+    chipSelect = 1;
   } else if (peekValue >= 0x40 && peekValue < 0x80) {
     offset = 0x4000;
+    chipSelect = 0;
   }
 
   if (romSize >= 32000) {
-    chipSelect = "CS12";
+    chipSelect = 2;
   }
 
   if(romSize >=64000){
     offset = 0;
+    chipSelect = 3;
   }
-  setupPinsForWrite();
+  setChipSelect(chipSelect);
+  setupDataPinsForWrite();
   Serial.print("\nStarting ROM Read with Offset ");
   Serial.print(offset, HEX);
   Serial.print(" and size ");
@@ -132,20 +272,19 @@ int writeFileToSRAM(const file_t &romFile){
   Serial.print("kb");
   long int startTime = millis();
   unsigned int address = 0;
-  assertReset();
+  
   assertWrite();
   romFile.seek(0);
-  while (address < romSize) {
+  while (romFile.available()) {
     unsigned int offsetAddress = address + offset;
     byte lowAddress = offsetAddress & 0xFF;
     byte highAddress = (offsetAddress >> 8) & 0xFF;
     byte data = romFile.read();
-    setDataPinValue(lowAddress);
+    setDataPinsValue(lowAddress);
     latchLowAddress();
-    setDataPinValue(highAddress);
+    setDataPinsValue(highAddress);
     latchHighAddress();
-    setDataPinValue(data);
-    latchData();
+    setDataPinsValue(data);
     selectRAM();
     deselectRAM();
     address++;
@@ -154,10 +293,9 @@ int writeFileToSRAM(const file_t &romFile){
   Serial.print("\nCompleted ROM Read in ");
   Serial.print(completeTime - startTime);
   Serial.print(" ms");
-  Serial.print("\nHanding over to MSX, use signal ");Serial.print(chipSelect);Serial.print("\n");
-
-
+  Serial.print("\nHanding over to MSX, using signal ");Serial.print(chipSelect);Serial.print("\n");  
 }
+
 
 void displayDirectoryContent(sd_t sd, file_t& aDirectory, byte tabulation) {
   file_t file;
@@ -190,90 +328,3 @@ void displayDirectoryContent(sd_t sd, file_t& aDirectory, byte tabulation) {
   }
   Serial.print("\n ");Serial.print(counter);Serial.print(" total files in directory");
 }
-
-sd_t initializeSDCard(int pin) {
-  Serial.print("Initializing SD card...");
-  sd_t sd;
-
-  if (!sd.begin(pin, SPI_FULL_SPEED)) {
-    Serial.println("initialization failed. Things to check:");
-    Serial.println("1. is a card inserted?");
-    Serial.println("2. is your wiring correct?");
-    Serial.println("3. did you change the chipSelect pin to match your shield or module?");
-    Serial.println("Note: press reset or reopen this serial monitor after fixing your issue!");
-    sd.initErrorHalt();
-  }
-
-  return sd;
-}
-void latchData() {
-  PORTC = (PORTC & 0x03) | B010000;
-  PORTC = (PORTC & 0x03) | B110000;
-}
-
-void latchLowAddress() {
-  PORTC = (PORTC & 0x03) | B001000;
-  PORTC = (PORTC & 0x03) | B101000;
-}
-
-void latchHighAddress() {
-  PORTC = (PORTC & 0x03) | B001100;
-  PORTC = (PORTC & 0x03) | B101100;
-}
-
-void handover() {
-  PORTC = (PORTC & 0x00) | B010111;
-}
-
-void selectRAM() {
-  PORTC = (PORTC & 0x03) | B000000;
-}
-
-void deselectRAM() {
-  PORTC = (PORTC & 0x03) | B100000;
-}
-
-void assertWrite() {
-  PORTC = (PORTC & B111100) | B000010;
-}
-
-void assertRead() {
-  PORTC = (PORTC & B111100) | B000001;
-}
-
-uint8_t readData(){
-  byte data = (PIND & B11111100)>>2;
-  return data | (((PINB & B00000011))<<6);
-}
-
-
-void assertReset() {
-  PORTC = (PORTC & 0x03) | B100100;
-  PORTC = (PORTC & 0x03) | B000100;
-  PORTC = (PORTC & 0x03) | B100100;
-}
-
-uint8_t readDataFromAddress(uint16_t address){
-  deselectRAM();
-  deassertReadandWrite();
-  setupPinsForWrite();
-  byte lowReadAddress = address & 0xFF;
-  byte highReadAddress = (address >> 8) & 0xFF;
-  setDataPinValue(lowReadAddress);
-  latchLowAddress();
-  setDataPinValue(highReadAddress);
-  latchHighAddress();
-  assertRead();
-  selectRAM();
-  setupPinsForRead();
-  return readData();
-}
-
-void deassertReadandWrite(){
-  PORTC = (PORTC & B111100) | B000011;
-}
-
-
-// File readFileFromSDCard(char* fileName) {
-//   return SD.open(fileName);
-// }
